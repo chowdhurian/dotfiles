d576c096ea39cc93120fcda11182c824ba3cf70f
Chowdhurian <manil.chowdhury@gmail.com>
2019-09-01 22:17:27 -0700

    Add script for fullscreen resize

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -23,3 +23,16 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
   f.h = max.h
   win:setFrame(f)
 end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
