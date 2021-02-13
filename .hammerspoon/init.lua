fbb63af6b525a611c18d4d0b5fdc5bc5425703c6
Manil Chowdhury <manil.chowdhury@gmail.com>
2021-02-12 18:37:23 -0800

    Add method to centre Main window

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -62,3 +62,16 @@ hs.hotkey.bind({"cmd", "alt"}, "F", function()
   f.h = max.h
   win:setFrame(f)
 end)
+
+hs.hotkey.bind({"cmd", "alt"}, "M", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 4)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
